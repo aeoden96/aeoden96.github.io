50d3b0c97872e2e85db9c2dcf94c8a520883ec96
Mateo <mateo1559@gmail.com>
2024-05-01 11:42:13 +0200

    setup actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/modex",
+};
 
 export default nextConfig;
