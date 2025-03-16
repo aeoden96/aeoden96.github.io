f77ba01eb15bf304f5c311246671d8a1a8557d5d
mateo <mateo1559@gmail.com>
2025-03-16 18:04:50 +0100

    feat: Add new project images and enhance site content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Mateo | Software Engineer & Frontend Developer";
+export const SITE_DESCRIPTION =
+  "I'm a software engineer and frontend developer with a passion for building user-friendly and efficient web applications.";
